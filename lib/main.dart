459c274f372853d67fac5b0b9552a062393b6ae7
Hien Dao <hienqdao5@gmail.com>
2026-04-12 20:19:32 -0400

    Fix the bug in main.dart to initialized firebase before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'screens/task_list_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
